33f66329f0864ef37cba02e43f90bdb95febee8a
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-18 11:36:35 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-function initGalery(items){
-    const ulEl = document.querySelector(".gallery");
-console.log(ulEl);
-  let galleryElements =items.map(item => {
+const ulEl = document.querySelector(".gallery");
+function createGalleryElements(items){
+    let galleryElements =items.map(item => {
     let liEl =document.createElement('li');
     liEl.classList.add('gallery__item');
+
     let imgEl = document.createElement('img');
     imgEl.classList.add('gallery__image');
     imgEl.src = item.preview;
     imgEl.alt = item.description;
     imgEl.dataset.source = "large-image.jpg";
+
     let linkEl = document.createElement('a');
     linkEl.classList.add('gallery__link');
     linkEl.href = item.original;
-   linkEl.appendChild(imgEl);
-   liEl.appendChild(linkEl);
+    linkEl.appendChild(imgEl);
+    liEl.appendChild(linkEl);
     return liEl;
 });
 ulEl.append(...galleryElements);
 return ulEl;
 };
+function openModal (href){ console.log (href);
+    //let MODAL_TEMPLATE = ''
+   
+};
+//function createGalleryElements(items){
+//return ulEl;
+//}
 function onGalleryItemClick(event){
     let clickeditemEl = event.target.closest('.gallery__item');
     if(!clickeditemEl){
         return;
     };
+    //clickeditemEl.classlist.add('is-active');
+    //let currentActiveCard = document.querySelector('.gallery__item.is-active');
+    //if(currentActiveCard){
+     //   currentActiveCard.classList.remove('is-active');  
+    //};
+    let {href} = clickeditemEl.dataset;
+    openModal(href);console.log(clickeditemEl)
 };
 function bindEvents(ulEl){
     ulEl.addEventListener('click',onGalleryItemClick);
@@ -35,5 +49,5 @@ function initGallery(items){
   let ulEl = createGalleryElements(items);
   bindEvents(ulEl); 
 }
-initGalery(galleryItems);
+initGallery(galleryItems);
 
