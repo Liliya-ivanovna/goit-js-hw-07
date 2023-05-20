90fee8408cf289bdcb529bbdd3c3d32d9e8b0006
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-20 20:54:32 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 const ulEl = document.querySelector(".gallery");
-function createGalleryElement(items){
-    let galleryElements =items.map(item => {
-    let liEl =document.createElement('li');
-    liEl.classList.add('gallery__item');
-
-    let imgEl = document.createElement('img');
-    imgEl.classList.add('gallery__image');
-    imgEl.src = item.preview;
-    imgEl.alt = item.description;
-    imgEl.dataset.source = item.original;
-
-    let linkEl = document.createElement('a');
-    linkEl.classList.add('gallery__link');
-    linkEl.href = item.original;
-    linkEl.appendChild(imgEl);
-    liEl.appendChild(linkEl);
-    return liEl;
-});
-ulEl.append(...galleryElements);
-return ulEl;
-};
-function onGalleryItemClick(event){
-    if(!event.target.classList.contains('gallery__image')){
-        return;
-    } console.log('click')
-        event.preventDefault();
-};
-function bindEvents(){
-    ulEl.addEventListener('click',onGalleryItemClick);
-    console.log('click')
-};
-function initGallery(items){
-  let ulEl = createGalleryElement(items);
-  bindEvents(ulEl); 
+function createGalleryMarkUp(items){
+   return items.map((item)=>
+   `<li class="gallery__item">
+   <a class="gallery__link" 
+   href ="${item.original}">
+   <img
+   class="gallery__image" 
+   src="${item.preview}"
+   data-source="${item.original}"
+   alt="${item.description}"/></a></li>`).join("");
 }
-initGallery(galleryItems);
+const addGalleryMarkUp = createGalleryMarkUp(galleryItems);
+ulEl.innerHTML = addGalleryMarkUp;
+ulEl.addEventListener("click",onGalleryItemClick);
+function onGalleryItemClick(event){
+    event.preventDefault();
+    if(event.target.nodeName !== "IMG"){
+        return;
+    }
+     const instance = basicLightbox.create(`<img src=
+      "${event.target.dataset.source}"
+       width="800" height ="600"/>`);
+      instance.show();
+
+ulEl.addEventListener("keydown",(event) =>{
+   if(event.code === "Escape"){
+    instance.close();
+   } 
+ });
+ }
+
+
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+function createGalleryMarkUp(items){
+   return items.map((item)=>
+   `<li class="gallery__item">
+   <a class="gallery__link" 
+   href ="${item.original}">
+   <img
+   class="gallery__image" 
+   src="${item.preview}"
+   alt="${item.description}"/></a></li>`).join(" ");
+}
+const addGalleryMarkUp = createGalleryMarkUp(galleryItems);
+ulEl.innerHTML = addGalleryMarkUp;
